1e7ddf48c462d18134765087bb7fdc8e41ad4d08
Avanish.K.a <k.a.avanish@gmail.com>
2018-09-13 19:05:06 +0530

    Google Scraping

--- Scraping.py
+++ Scraping.py
@@ -2,13 +2,13 @@
 
 
 import requests,bs4
-a=str(input())
-b=[('q',a)]
-url="https://google.com/search?"
-c=requests.get(url=url,params=b)
-d=bs4.BeautifulSoup(c.text,'lxml')
-titles=d.find_all("h3")
-x=0;
+a = str(input())
+b = [('q',a)]
+url = "https://google.com/search?"
+c = requests.get(url=url,params=b)
+d = bs4.BeautifulSoup(c.text,'lxml')
+titles = d.find_all("h3")
+x = 0;
 for title in titles:
-    x=x+1;
+    x = x+1;
     print(title.get_text())
